2e71461e18382a2d4161faf3dff594667d7b4f2b
Latif <latifyahia98@gmail.com>
2021-03-23 19:09:32 +0000

    made code neat

--- App.js
+++ App.js
@@ -4,157 +4,129 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MyChart } from './MyChart';
 import { YellowBox } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-YellowBox.ignoreWarnings([ // https://reactnavigation.org/docs/troubleshooting/
-  'Non-serializable values were found in the navigation state',
-]);
+YellowBox.ignoreWarnings(['Non-serializable values were found in the navigation state',]);
+
+
 //const weatherData=[{''}];
 function HomeScreen({ navigation, route })
 {
-
   const [text, onChangeText] = React.useState(null);
   const [weatherData, setWeatherData] = React.useState("hello");
   const getWeatherDataFromApi = () =>
   {
-     fetch('https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=1f3b675bf27e4e2e0ec49c0f6a5bc146')
-      .then((response) => response.json())
-      .then((json) => {
-        if(json){
-          setWeatherData(json.weather);
-          console.log(json.weather);
-          console.log(weatherData);
-        }
-        else(
-          console.log("error")
-        )
-      })
-      .catch((error) => {
-        console.error("error");
-      });
-    }
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=1f3b675bf27e4e2e0ec49c0f6a5bc146')
+    .then((response) => response.json())
+    .then((json) =>
+    {
+      if(json)
+      {
+        setWeatherData(json.weather);
+        console.log(json.weather);
+        console.log(weatherData);
+      }
+      else(console.log("error"))
+    })
+    .catch((error) =>
+    {
+      console.error("error");
+    });
+  }
 
 
-  return (
-    <View style={{ flex: 1,
-                   alignItems: 'center',
-                   justifyContent: 'center',
-                   backgroundColor: '#fff',
-                   paddingBottom: 50
-                 }}>
+  return(
+    <View style={{ flex: 1,alignItems: 'center',justifyContent: 'center',backgroundColor: '#fff',paddingBottom: 50}}>
 
       <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#778899' }}>
-
-      <TextInput
-              style={styles.input}
-              onChangeText={onChangeText}
-              value={text}
-              placeholder="Please Enter The Name Of The City"
-      />
-
-
-      <Image source={{uri: 'https://scx2.b-cdn.net/gfx/news/hires/2019/weatherforec.jpg'}}
-               style={{width: 400, height: 350}} />
-        <Text style={{ margin: 10 ,color:'#000',padding: 5,backgroundColor: '#fff',borderColor: '#000', borderWidth: 2, borderRadius: 10}}>Weather Data:
-        </Text>
-
+        <TextInput style={styles.input} onChangeText={onChangeText} value={text} placeholder="Please Enter The Name Of The City"/>
+        <Image source={{uri: 'https://scx2.b-cdn.net/gfx/news/hires/2019/weatherforec.jpg'}} style={{width: 400, height: 350}} />
+        <Text style={{ margin: 10 ,color:'#000',padding: 5,backgroundColor: '#fff',borderColor: '#000', borderWidth: 2, borderRadius: 10}}>Weather Data:</Text>
       </View>
+
       <View style={{flexDirection:'row'}}>
-      <View style={{ padding: 5, backgroundColor: '#FFFF33', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
-      <Text style ={{fontSize:20}}onPress={() => navigation.navigate('ChartTheDataToday')}>
-      Search Weather
-      </Text>
+        <View style={{ padding: 5, backgroundColor: '#FFFF33', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
+          <Text style ={{fontSize:20}}onPress={() => navigation.navigate('ChartTheDataToday')}>Search Weather</Text>
+        </View>
+        <View style={{ padding: 5, backgroundColor: '#1E90FF', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
+          <Text style ={{fontSize:20}}onPress={() => navigation.navigate('ChartTheData')}>Search Forecast</Text>
+        </View>
+        <View style={{ padding: 5, backgroundColor: '#0f0', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
+          <Text style ={{fontSize:20}} onPress={getWeatherDataFromApi}>Search</Text>
+        </View>
       </View>
-      <View style={{ padding: 5, backgroundColor: '#1E90FF', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
-    <Text style ={{fontSize:20}}onPress={() => navigation.navigate('ChartTheData')}>
-      Search Forecast
-    </Text>
 
     </View>
-    <View style={{ padding: 5, backgroundColor: '#0f0', marginBottom: 10, marginTop: 10, borderColor: '#fff', borderWidth: 2, borderRadius: 10, }} >
-    <Text style ={{fontSize:20}} onPress={getWeatherDataFromApi}>
-      Search
-    </Text>
-
-    </View>
-    </View>
-    </View>
-
   );
 }
 
-function ChartTheData({ navigation, route }) {
+function ChartTheData({ navigation, route })
+{
 //  if(route.params.show == false){
   //  return null;
   //}
-
-  let lineData = {interpolation: 'T', data: [10, 12, 8, 9, 10, 5, 0, -5],
-                  nativeData: {
-                    labels: ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat","Sun"],
-                    datasets: [
-                    {
-                      data: [
-                        Math.random() * 90,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100,
-                        Math.random() * 100
-                      ]
-                    }
-                  ]
-                }
+  let lineData =
+  {
+    interpolation: 'T', data: [10, 12, 8, 9, 10, 5, 0, -5],
+    nativeData:
+    {
+      labels: ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat","Sun"],
+      datasets:
+      [{
+      data:
+        [
+          Math.random() * 90,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100
+        ]
+      }]
+    }
   };
-  return (
-    <View style={{ flex: 1,
-                   justifyContent: 'center',
-                   alignItems: 'center',
-                   backgroundColor: '#1e90ff',
-                   paddingBottom: 50
-                 }}>
-      <View style={{ backgroundColor: '#ffffff', borderColor: '#000000', borderWidth: 2, padding: 2 }} >
+  return(
+    <View style={{ flex: 1,justifyContent: 'center', alignItems: 'center',backgroundColor: '#1e90ff',paddingBottom: 50}}>
+
+      <View style={{ backgroundColor: '#ffffff', borderColor: '#000000', borderWidth: 2, padding: 2 }}>
         <MyChart dataToChart = {lineData}> </MyChart>
       </View>
-      <View style={{ padding: 5, marginTop: 10, backgroundColor: '#c0c0c0', borderColor: '#6060ff', borderWidth: 2, borderRadius: 10, }} >
-        <Text onPress={() => navigation.goBack()}>
-          Done (back to Home screen)
-        </Text>
+
+      <View style={{ padding: 5, marginTop: 10, backgroundColor: '#c0c0c0', borderColor: '#6060ff', borderWidth: 2, borderRadius: 10, }}>
+        <Text onPress={() => navigation.goBack()}>Done (back to Home screen)</Text>
       </View>
+
     </View>
   );
 }
 
-function ChartTheDataToday({ navigation, route }) {
- const [text, onChangeText] = React.useState(null);
+function ChartTheDataToday({ navigation, route })
+{
+  const [text, onChangeText] = React.useState(null);
+  return(
+    <View style={{ flex: 1,alignItems: 'center',justifyContent: 'center',backgroundColor: '#fff', paddingBottom: 50}}>
 
-  return (
-    <View style={{ flex: 1,
-                   alignItems: 'center',
-                   justifyContent: 'center',
-                   backgroundColor: '#fff',
-                   paddingBottom: 50
-                 }}>
       <View style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
-
-        <Image source={{uri: 'https://scx2.b-cdn.net/gfx/news/hires/2019/weatherforec.jpg'}}
-               style={{width: 400, height: 350}} />
-
+        <Image source={{uri: 'https://scx2.b-cdn.net/gfx/news/hires/2019/weatherforec.jpg'}} style={{width: 400, height: 350}}/>
       </View>
+
       <View style={{flexDirection:'row'}}>
+      </View>
 
-
-
-    </View>
     </View>
   );
 }
-const Stack = createStackNavigator();
 
-export default function App() {
-  return (
+const Stack = createStackNavigator();
+export default function App()
+{
+  return(
     <NavigationContainer>
       <Stack.Navigator mode="modal">
+
+
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -169,6 +141,8 @@ export default function App() {
             }
           }}
         />
+
+
         <Stack.Screen
           name="ChartTheData"
           component={ChartTheData}
@@ -185,6 +159,8 @@ export default function App() {
             }
           }}
         />
+
+
         <Stack.Screen
           name="ChartTheDataToday"
           component={ChartTheDataToday}
@@ -201,6 +177,8 @@ export default function App() {
             }
           }}
         />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
